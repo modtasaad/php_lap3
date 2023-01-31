60fd87d1678a9864d236ef7b087670cae9f252fd
modtasaad <117727228+modtasaad@users.noreply.github.com>
2023-01-31 20:16:37 +0200

    Update eyeicon.js

--- eyeicon.js
+++ eyeicon.js
@@ -16,30 +16,3 @@ pwshowhide.forEach(eyeicon =>{
    })
 
 })
-
-// const form = document.querySelector(".formt"),
-
- 
-function emptyvalidation(){
-    const input =document.getElementById("name");
-    if(input.values===""){
-        return input.classList.add("invalid");//inputField.style.border="1px solid red";
-        
-    }
-    return input.classList.remove("invalid");//inputField.style.border="none";
-}
-
-// form.addEventListener("submit" , (e)=>{
-//     e.preventDefault();    
-//     emptyvalidation();
-//     // calling fun in key up
-//     input.addEventListener("keyup",emptyvalidation()); 
-// })
-
-// function validateForm() {
-//     var x = document.forms["myform"]["name"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//   }
